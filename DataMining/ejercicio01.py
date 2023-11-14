7a462c5f211e0599db85141f35d76ac01b84a438
AlejandroTC <aletamayo-02@hotmail.com>
2023-11-14 09:35:39 -0600

    convolucion e inicio DM

---
+++ DataMining/ejercicio01.py
@@ -0,0 +1,14 @@
+# Importa las bibliotecas necesarias
+import pandas as pd
+import numpy as np
+
+# Exploración de Datos Iniciales:
+# • Descarga el conjunto de datos de Kaggle y cárgalo en tu entorno de trabajo.
+data = pd.read_csv("DataMining/healthcare_dataset.csv")
+# • Explora la estructura del conjunto de datos y verifica la calidad de los datos.
+print(f"\n Data Head")
+print(data.head())  
+print(f"\n Data Info")
+print(data.info())  
+print(f"\n Data Describe")
+print(data.describe())  
--- ImageAnalysis/imageanalisys/src/main/java/espacial/Convolucion.java
+++ ImageAnalysis/imageanalisys/src/main/java/espacial/Convolucion.java
@@ -1,5 +1,6 @@
 package espacial;
 
+import java.awt.Color;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
 
@@ -18,7 +19,44 @@ public class Convolucion{
 
     //Convolucion simple
     public Image convolucionar(int[][] mascara, int div){
-        
+        Color nuevo;
+        for (int x=0; x < this.ImagenOriginal.getWidth();x++)
+        {
+            for(int y=0; y < this.ImagenOriginal.getHeight(); y++){
+                int rgb = convolucionar(x, y, mascara);
+                nuevo = new Color(rgb);
+                this.ImagenOriginal.setRGB(x, y, nuevo.getRGB());
+            }
+        }
         return null;
+    }
+
+    private int convolucionar(int x, int y, int[][] mascara) {
+        for (int j=0; j < mascara.length ; j++ ){
+            for (int m=0; m < mascara.length ; m++){
+                //verificar si la mascara es diferente a 0
+                if(mascara[j][m] != 0){
+                    //
+                }
+            }
+        }
+        return 0;
+    }
+
+    private int obtenerRGB(int x, int y, int j){
+        int rgb = 0;
+        switch (j) {
+            case 0:
+            if(x-1<0 || y-1<0) return 0;
+            if ((x-1>this.ImagenOriginal.getHeight()) || (y-1>this.ImagenOriginal.getWidth())) return 0;
+
+            rgb = this.ImagenOriginal.getRGB(x-1, y-1);
+
+                break;
+        
+            default:
+                break;
+        }
+        return rgb;
     }
 }
