8efb4a466ce06b59e3c3d722f9783b1d741214f8
AlejandroTC <aletamayo-02@hotmail.com>
2023-11-22 10:00:13 -0600

    ConvolucionP08

---
+++ DataMining/practica08.py
@@ -0,0 +1,96 @@
+# Importa las bibliotecas necesarias
+from matplotlib import pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, precision_score, recall_score
+
+
+# Carga de datos
+data = pd.read_csv('DataMining/BreastCancerWisconsin.csv')
+# Exploracion de datos
+print("Exploración de Datos/n")
+print(f"\n Data Head")
+print(data.head())  
+print(f"\n Data Info")
+print(data.info())  
+print(f"\n Data Describe")
+print(data.describe())  
+
+# Preprocesamientoo de datos
+# Transformacion de Diagnosis a Binario
+data['diagnosis'] = data['diagnosis'].replace('B', 0).replace('M', 1)
+
+# Exploracion de datos
+print("Exploración de Datos procesados\n")
+print(f"\n Data Head")
+print(data.head())  
+print(f"\n Data Info")
+print(data.info())  
+print(f"\n Data Describe")
+print(data.describe())  
+
+# Analizar correlacion
+# Matriz de correlacion
+predictor_variables = ["radius_mean","texture_mean","perimeter_mean","area_mean","smoothness_mean","compactness_mean","concavity_mean","concave points_mean","symmetry_mean","fractal_dimension_mean","radius_se","texture_se","perimeter_se","area_se","smoothness_se","compactness_se","concavity_se","concave points_se","symmetry_se","fractal_dimension_se","radius_worst","texture_worst","perimeter_worst","area_worst","smoothness_worst","compactness_worst","concavity_worst","concave points_worst","symmetry_worst","fractal_dimension_worst"]
+target_variable = 'diagnosis'
+correlation_matrix = data[predictor_variables + [target_variable]].corr()
+# plt.figure(figsize=(10, 6))
+# sns.barplot(x=correlation_matrix.index, y=correlation_matrix['diagnosis'], palette="viridis")
+# plt.title('Correlación con Diagnostico')
+# plt.xlabel('Variables Predictoras')
+# plt.ylabel('Correlación')
+# plt.xticks(rotation=45)
+# plt.show()
+print("\nMatriz de correlación:\n", correlation_matrix)
+# Variables con correlacion más fuertes
+strong_correlations = correlation_matrix['diagnosis'][abs(correlation_matrix['diagnosis']) > 0.5].index
+print(f"\nCorrelaciones más fuertes (>0.1):\n",strong_correlations)
+
+# Dividir el conjunto de datos
+X = data[strong_correlations].drop('diagnosis', axis=1)
+y = data['diagnosis']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+#Modelo
+def sigmoid(z):
+    return 1 / (1 + np.exp(-z))
+
+def train_logistic_regression(X, y, learning_rate=0.01, max_iterations=1, convergence_criterion=0.0001):
+    # Inicializar pesos con valores pequeños o aleatorios
+    W = np.random.randn(X.shape[1])
+    iterations = 0
+    converged = False
+
+    while iterations < max_iterations and not converged:
+        z = np.dot(X, W)
+        y_pred = sigmoid(z)
+        error = y - y_pred
+        gradient = np.dot(X.T, error)
+        W += learning_rate * gradient
+        if np.linalg.norm(learning_rate * gradient) < convergence_criterion:
+            converged = True
+        iterations += 1
+    return W
+
+def predict(X_new, W):
+    z_new = np.dot(X_new, W)
+    y_pred_new = sigmoid(z_new)
+    predicted_class = 1 if y_pred_new >= 0.5 else 0
+    return predicted_class
+
+
+# Entrenar el modelo
+W_trained = train_logistic_regression(X_train, y_train)
+
+# Hacer predicciones en el conjunto de prueba
+predictions = [predict(x, W_trained) for x in X_test]
+
+accuracy = accuracy_score(y_test, predictions)
+precision = precision_score(y_test, predictions)
+recall = recall_score(y_test, predictions)
+
+print(f'Accuracy: {accuracy:.2f}')
+print(f'Precision: {precision:.2f}')
+print(f'Recall: {recall:.2f}')
--- ImageAnalysis/imageanalisys/src/main/java/espacial/Convolucion.java
+++ ImageAnalysis/imageanalisys/src/main/java/espacial/Convolucion.java
@@ -3,110 +3,138 @@ package espacial;
 import java.awt.Color;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
+import com.imageanalisys.HerramientasImagen;
 
 /*
  * ToDo: tenemos que ddeterminar el divisior
  * offset: es un valor que se añade al resultante
  */
 
-public class Convolucion{
-    //Mascara de deteccion de bordes
-    BufferedImage ImagenOriginal;
+public class Convolucion {
+    // mascaras de deteccion de bordes
+    BufferedImage imagenOriginal;
 
-    public Convolucion(BufferedImage ImagenOriginal){
-        this.ImagenOriginal = ImagenOriginal;
+    public Convolucion(BufferedImage imagenOriginal) {
+        this.imagenOriginal = imagenOriginal;
     }
-
-    //Convolucion simple
-    public Image convolucionar(int[][] mascara, int div){
+    // convolucion simple    
+    public Image convolucionar(int[] mascara, int div){
         Color nuevo;
-        for (int x=0; x < this.ImagenOriginal.getWidth();x++)
-        {
-            for(int y=0; y < this.ImagenOriginal.getHeight(); y++){
-                int rgb = convolucionar(x, y, mascara, div);
+        // Color f = new Color(this.imagenOriginal.getRGB(0,0));
+        // System.out.println();
+        // el proceso de convolucion se hace para todos los pixeles
+        for(int x=0;x<this.imagenOriginal.getWidth();x++){
+            for(int y=0;y<this.imagenOriginal.getHeight();y++){
+                // tenemos un nuevo tono
+                int rgb = convolucionar(x,y,mascara,div);
                 nuevo = new Color(rgb);
-                this.ImagenOriginal.setRGB(x, y, nuevo.getRGB());
+                this.imagenOriginal.setRGB(x, y, nuevo.getRGB());
             }
         }
-        return null;
-    }
+        return HerramientasImagen.toImage(imagenOriginal);
+    }    
 
-    private int convolucionar(int x, int y, int[][] mascara, int div) {
-        // for (int j=0; j < mascara.length ; j++ ){
-        //     for (int m=0; m < mascara.length ; m++){
-        //         //verificar si la mascara es diferente a 0
-        //         if(mascara[j][m] != 0){
-        //             //
-        //         }
-        //     }
-        // }
+    private int convolucionar(int x, int y, int[] mascara, int div) {
+    //    //recorremos la mascara
+    //    for(int j=0;j<mascara.length;j++){
+    //        for(int m=0;m<mascara.length;m++){
+    //            // verificar si la mascara es diferente a 0
+    //            if(mascara[j][m]!=0){
+    //                // convolucionar
+    //            }
+    //        }
+    //    }
         Color aux;
-        int aR=0, aG=0, aB=0;
-        for (int j=0; j<9; j++)
-        {
-            int rgb = obtenerRGB(x, y, j);
-            if(rgb != 0)
-            {
+        int aR=0,aG=0,aB=0;
+        for(int j=0; j < mascara.length;j++){
+            // canales     
+            int rgb = obtenerRGB(x,y,j);
+            if(rgb!=0){
                 aux = new Color(rgb);
-                aR+=aux.getRed()*mascara[j][0];
-                aG+=aux.getGreen()*mascara[j][0];
-                aB+=aux.getBlue()*mascara[j][0];
-
+                aR+=aux.getRed()*mascara[j];
+                aG+=aux.getGreen()*mascara[j];
+                aB+=aux.getBlue()*mascara[j];
             }
         }
-        aR /= div;
-        aG /= div;
-        aB /= div;
+        // evaluar la división
+        aR/=div;
+        aG/=div;
+        aB/=div;
         aux = new Color(aR, aG, aB);
         return aux.getRGB();
     }
 
-    private int obtenerRGB(int x, int y, int j){
+    private int obtenerRGB(int x, int y, int j) {
         int rgb = 0;
-        switch (j) {
-            case 0:
-                if(x-1<0 || y-1<0) return 0;
-                if ((x-1>this.ImagenOriginal.getHeight()) || (y-1>this.ImagenOriginal.getWidth())) return 0;
-                rgb = this.ImagenOriginal.getRGB(x-1, y-1);
+        switch (j){
+            case 0:{
+                if((x-1<0) || (y-1<0)) return 0;
+                if((x-1>=this.imagenOriginal.getHeight())
+                        ||(y-1>=this.imagenOriginal.getWidth()))return 0; 
+                rgb = this.imagenOriginal.getRGB(x-1, y-1);
                 break;
-            case 1:
-                if(x-1<0 || y<0) return 0;
-                if ((x-1>this.ImagenOriginal.getHeight()) || (y>this.ImagenOriginal.getWidth())) return 0;
-                rgb = this.ImagenOriginal.getRGB(x-1, y);
+            }
+            case 1:{
+                if((x-1<0) || (y<0)) return 0;
+                if((x-1>=this.imagenOriginal.getHeight())
+                        ||(y>=this.imagenOriginal.getWidth()))return 0; 
+                rgb = this.imagenOriginal.getRGB(x-1, y);
                 break;
-            case 2:
-                if(x-1<0 || y+1<0) return 0;
-                if ((x-1>this.ImagenOriginal.getHeight()) || (y+1>this.ImagenOriginal.getWidth())) return 0;
-                rgb = this.ImagenOriginal.getRGB(x-1, y+1);
+            }
+            case 2:{
+                if((x-1<0) || (y+1<0)) return 0;
+                if((x-1>=this.imagenOriginal.getHeight())
+                        ||(y+1>=this.imagenOriginal.getWidth()))return 0; 
+                rgb = this.imagenOriginal.getRGB(x-1, y+1);
                 break;
-            case 3:
-                if(x<0 || y-1<0) return 0;
-                if ((x>this.ImagenOriginal.getHeight()) || (y-1>this.ImagenOriginal.getWidth())) return 0;
-                rgb = this.ImagenOriginal.getRGB(x, y-1);
+            }
+            case 3:{
+                if((x<0) || (y-1<0)) return 0;
+                if((x>=this.imagenOriginal.getHeight())
+                        ||(y-1>=this.imagenOriginal.getWidth()))return 0; 
+                rgb = this.imagenOriginal.getRGB(x, y-1);
                 break;
-            case 4:
-                if(x<0 || y+1<0) return 0;
-                if ((x>this.ImagenOriginal.getHeight()) || (y+1>this.ImagenOriginal.getWidth())) return 0;
-                rgb = this.ImagenOriginal.getRGB(x, y+1);
+            }
+            
+            case 4:{
+                if((x<0) || (y<0)) return 0;
+                if((x>=this.imagenOriginal.getHeight())
+                        ||(y>=this.imagenOriginal.getWidth()))return 0; 
+                rgb = this.imagenOriginal.getRGB(x, y);
                 break;
-            case 5:
-                if(x+1<0 || y-1<0) return 0;
-                if ((x+1>this.ImagenOriginal.getHeight()) || (y-1>this.ImagenOriginal.getWidth())) return 0;
-                rgb = this.ImagenOriginal.getRGB(x+1, y-1);
+            }
+            
+            case 5:{
+                if((x<0) || (y+1<0)) return 0;
+                if((x>=this.imagenOriginal.getHeight())
+                        ||(y+1>=this.imagenOriginal.getWidth()))return 0; 
+                rgb = this.imagenOriginal.getRGB(x, y+1);
                 break;
-            case 6:
-                if(x+1<0 || y<0) return 0;
-                if ((x+1>this.ImagenOriginal.getHeight()) || (y>this.ImagenOriginal.getWidth())) return 0;
-                rgb = this.ImagenOriginal.getRGB(x+1, y);
+            }
+            
+             case 6:{
+                if((x+1<0) || (y-1<0)) return 0;
+                if((x+1>=this.imagenOriginal.getHeight())
+                        ||(y-1>=this.imagenOriginal.getWidth()))return 0; 
+                rgb = this.imagenOriginal.getRGB(x+1, y-1);
                 break;
-            case 7:
-                if(x+1<0 || y+1<0) return 0;
-                if ((x+1>this.ImagenOriginal.getHeight()) || (y+1>this.ImagenOriginal.getWidth())) return 0;
-                rgb = this.ImagenOriginal.getRGB(x+1, y+1);
+            }
+             
+            case 7:{
+                if((x+1<0) || (y<0)) return 0;
+                if((x+1>=this.imagenOriginal.getHeight())
+                        ||(y>=this.imagenOriginal.getWidth()))return 0; 
+                rgb = this.imagenOriginal.getRGB(x+1, y);
                 break;
-            default:
-                rgb = this.ImagenOriginal.getRGB(x, y);
+            }
+            
+            case 8:{
+                if((x+1<0) || (y+1<0)) return 0;
+                if((x+1>=this.imagenOriginal.getHeight())
+                        ||(y+1>=this.imagenOriginal.getWidth()))return 0; 
+                rgb = this.imagenOriginal.getRGB(x+1, y+1);
                 break;
+            }   
         }
         return rgb;
     }
